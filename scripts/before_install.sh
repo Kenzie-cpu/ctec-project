4c4e976135b808ea4b024003e6547712c0a03c62
addsw <addsw@hotmail.co.uk>
2022-08-18 23:56:02 +0800

    update

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 #download node and npm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash . ~/.nvm/mvm.sh
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash 
 nvm install node
+nvm install 16
 
 # create working directly if it does not exists
 DIR="/home/ec2-user/express-app"
